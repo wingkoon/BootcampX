dcce7c8bf36079e0efe53e7a0a810a3951021c4e
wingkoon <wingkoon@yahoo.com>
2023-12-07 20:54:47 -0800

    student and teacher query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -22,6 +22,20 @@ FROM students
 LIMIT 5;
 `)
 .then(res => {
+  console.log(res);
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
+
+
+
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
   })
@@ -39,6 +53,20 @@ LIMIT 5;
   })
 });
 
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+/*
 const queryString = `
   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
   FROM students
@@ -46,11 +74,11 @@ const queryString = `
   WHERE cohorts.name LIKE $1
   LIMIT $2;
   `;
-
+/*
   const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
 // Store all potentially malicious values in an array.
 const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values);
-
+*/
